3f25e6e06ede9a7dff08a38d03683f492e6af8e2
Prabesh01 <prabesh01@pm.me>
2025-09-23 00:49:20 +0545

    fe: hor -> party: 50% done

--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Candidate, Case, Kartut, Party,  Representative
+from django.db.models import Count
 
 def home(request):
     return render(request, 'home.html')
@@ -6,8 +8,20 @@ def home(request):
 def hor_home(request, year):
     return render(request, 'hor_home.html', {'year': year})
 
-def hor_parties(request, year):
-    pass
-
 def hor_constituencies(request, year):
     return render(request, 'hor_constituencies.html', {'year': year})
+
+def hor_fptp_candidate_detail(request, year, constituency, candidate_id):
+    pass
+
+def hor_parties(request, year):
+    parties = Party.objects.filter(
+        representative__year=year, # representative__proportional=True
+    ).annotate(
+        rep_count=Count('representative')
+    ).distinct().order_by('-rep_count', 'name')
+
+    return render(request, 'party_list.html', {'year': year, 'parties': parties})
+
+def hor_pr_candidate_detail(request, year, party, candidate_id):
+    pass
