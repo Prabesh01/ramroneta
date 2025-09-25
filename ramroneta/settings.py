c7d1182452778ffe0ae8c2fea8229f40937fe6c0
Prabesh01 <prabesh01@pm.me>
2025-09-25 10:22:29 +0545

    nginx deployment with gunicorn. debug off

--- ramroneta/settings.py
+++ ramroneta/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%j_)tds3i!qndh6i*6&&p%)+@$(svkqcu^!c$1scpa3ctq9k3v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['hamroneta.info']
+CSRF_TRUSTED_ORIGINS = ['https://hamroneta.info']
 
 # Application definition
 
